15f2185c3adf3d8aa96684114042e0b49b1ec0cb
Artem Rumbens <RumbensArtem@yandex.ru>
2025-10-09 11:19:43 +0300

    Fix linters

--- src/pages/ArticlesPage/ui/ArticlesPage.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import cls from './ArticlesPage.module.scss'
+import { useTranslation } from "react-i18next";
 
 export const ArticlesPage:React.FC=()=>{
+
+    const {t}=useTranslation()
+
     return(
         <div className={cls.ArticlePage}>
-            ArticlePage
+            {t('ArticlePage')}
         </div>
     )
 }
