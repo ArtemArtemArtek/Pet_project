f27fa2903c4e41ffb1799eeb67f67c58cb48bf39
Artem Rumbens <RumbensArtem@yandex.ru>
2025-11-06 09:43:44 +0300

    Fix

--- src/shared/api/instanceApi.tsx
+++ src/shared/api/instanceApi.tsx
@@ -3,12 +3,12 @@ import { LOCAL_USER_AUTH_KEY, BASE_SERVER_URL } from "../consts/consts";
 
 export const $api = axios.create({
     baseURL: BASE_SERVER_URL,
-    headers: { 'authorization': localStorage.getItem(LOCAL_USER_AUTH_KEY)||'' }
+    headers: { 'authorization': localStorage.getItem(LOCAL_USER_AUTH_KEY)}
 })
 
-$api.interceptors.request.use((config)=>{
-    if(config.headers){
-        config.headers.Authorization = localStorage.getItem(LOCAL_USER_AUTH_KEY)||''
-    }
-    return config
-})
+// $api.interceptors.request.use((config)=>{
+//     if(config.headers){
+//         config.headers.Authorization = localStorage.getItem(LOCAL_USER_AUTH_KEY)
+//     }
+//     return config
+// })
